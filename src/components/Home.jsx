f7f24290434868ab37683c3d03f6410222318480
khadijah <phunmilawal@gmail.com>
2024-03-28 22:35:36 +0100

    Task: Changes in font color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 function Home() {
   return (
+    <>
     <div className="text-box">
-      <h1>Eco-friendly Waste Solution <br />
-      at Your Finger tips</h1>
-      <p>Get Rid, Get Green, Go Clean!</p>
-       
+      <h1>Eco-friendly Waste <br />
+      Solution at Your <br />
+      Finger tips</h1>
     </div>
+    <p>Get Rid, Get Green, Go Clean!</p>
+    </>
   );
 }
 export default Home;
