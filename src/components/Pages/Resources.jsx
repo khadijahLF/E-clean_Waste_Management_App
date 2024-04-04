78da6d057d5398a1907173d98d4b517d4df32380
khadijah <phunmilawal@gmail.com>
2024-04-04 13:51:00 +0100

    feat: ResourcePage Blogs

--- src/components/Pages/Resources.jsx
+++ src/components/Pages/Resources.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import HomeNav from '../Layouts/HomeNav'
+import Footer from '../Layouts/Footer'
+import Blog from '../Layouts/Blog'
 
 function Resources() {
   return (
-    <div>
+   
+      <>
       <HomeNav/>
-    </div>
+      <div className="container-resources">
+            <div className="content-resources">
+            <h1>Resources</h1>
+            <p>All you need to effectively manage your waste</p>
+            </div>
+      </div>
+      <Blog/>
+      <Footer/>
+      </>
+    
+    
   )
 }
 
