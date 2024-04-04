0b6a8519c12b117d4ba3ee9591c2c7d6c3eb2bf4
khadijah <phunmilawal@gmail.com>
2024-04-04 12:22:02 +0100

    feat: Button Hover Fix

---
+++ src/components/Pages/Resources.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeNav from '../Layouts/HomeNav'
+
+function Resources() {
+  return (
+    <div>
+      <HomeNav/>
+    </div>
+  )
+}
+
+export default Resources
