553b0df21cb31eb478272918700618a2dd869d31
kyenpya <kyenpyagutap@gmail.com>
2024-04-10 10:32:02 +0100

    Task: added the team photos container

--- src/components/Layouts/MeetTeam.jsx
+++ src/components/Layouts/MeetTeam.jsx
@@ -9,6 +9,7 @@ function MeetTeam(params) {
         <div className="team-text">
           <p>We are a team of passionate individuals dedicated to making a positive impact on our communities and the planet. What unites us is our shared vision of a greener, more prosperous Africa, where waste is no longer a burden but a valuable resource waiting to be tapped.</p>
         </div>
+        <div className="team-pictures"></div>
       </section>
     </>
   );
