1ea603c88993fa835583b4858cbf18f29328ac7f
kyenpya <kyenpyagutap@gmail.com>
2024-04-11 15:32:14 +0100

    Task: Made the 'about us' page pixel perfect. well, almost!

--- src/components/Layouts/OurMission.jsx
+++ src/components/Layouts/OurMission.jsx
@@ -13,13 +13,13 @@ function OurMission() {
 
           <h3>Bin It Right, Keep the Planet Bright</h3>
           <p>
-            At EClean, we are on a mission to revolutionize waste management
-            practices across Africa. We believe that every piece of waste holds
-            the potential for transformation, not just in terms of environmental
-            sustainability but also as a source of economic opportunity. We aim
-            to bridge the gap between waste generators and recyclers, creating a
-            seamless platform where responsible disposal leads to tangible
-            rewards
+            At EClean, we are on a mission to revolutionize waste 
+            management practices across Africa. We believe that every 
+            piece of waste holds the potential for transformation, not 
+            just in terms of environmental sustainability but also as a 
+            source of economic opportunity. We aim to bridge the gap 
+            between waste generators and recyclers, creating a seamless 
+            platform where responsible disposal leads to tangible rewards
           </p>
         </div>
       </div>
