99f86d00d0b68afa632713fa41d537ed58746454
kyenpya <kyenpyagutap@gmail.com>
2024-04-11 17:13:29 +0100

    TAsk: fixed the revolution man, made it correspond with sizes on figma

--- src/components/Layouts/Revolution.jsx
+++ src/components/Layouts/Revolution.jsx
@@ -6,26 +6,30 @@ const Revolution = () => {
     <>
 
     <div className='parent-revcontainer'> 
-        <div className="revolution-container">
+        {/* <div className="revolution-container">
             
 
-            <div className="revimage">
-                <img src="src\assets\medium-shot-smiley-man-with-helmet 1.png" alt="man" />
+            <div className="revimage"> */}
+                <img className="revimage" src="src\assets\medium-shot-smiley-man-with-helmet 1.png" alt="man" />
+                
+
             </div>
 
             <div className="content-container">
-
-                <div className='revPara'>
-                    <p className='underline_txt'>What We Do</p>
-                </div>
-
+            <h6>What We Do</h6>
                 <h2>Revolutionizing Waste <br/> Management Practices</h2>
                 <p>We believe that every piece of waste holds the potential for transformation, not just in terms of environmental sustainability but also as a source of economic opportunity. Our goal is to empower individuals and communities to earn from their recyclable items and to facilitate the seamless flow of these materials to recyclers who need them.</p>
                 <button>Learn more</button>
+
+                {/* <div className='revPara'>
+                    
+                </div> */}
+
+                
             </div>
 
-        </div>
-    </div>
+        {/* </div>
+    </div> */}
     </>
   );
 };
