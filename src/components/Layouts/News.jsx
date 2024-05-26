6abb633368fd2ee95e0a730810d74e6640abef09
khadijah <phunmilawal@gmail.com>
2024-05-26 21:36:38 +0100

    feat: responsiveness for mobile

--- src/components/Layouts/News.jsx
+++ src/components/Layouts/News.jsx
@@ -68,30 +68,30 @@ const News = () => {
    <div className='sm-newsContainer'>
    
 
-   <div className="sm-news-container">
-     <div className="sm-image-container">
-       <img src="/assets/Blogimage1.png" alt="News Image" />
-       <div className="imagetext">Trash Trek: Community Cleanup and <br/> Recycling Drive</div>
-     </div>
-     <div className="sm-text-container">
-       <div className="sm-row">
-         <div className="sm-row-item">
-           <img src="/assets/Blogimage2.png" alt="News Image" />
-               <div className='sm-blogandp'>
-                   <div className='sm-blog-title'>
-                       <p >Blog</p>
-                       <p className='sm-date'>21 FEB 2024</p>
-                   </div>
-                   <div className='sm-btit'>
-                   <p>10 Tips for Effective Waste Sorting at Home</p>
-                   </div>
-               </div>
-         </div>
-       </div>
-       
-    
-     </div>
-   </div>
+    <div className="sm-news-container">
+        <div className="sm-image-container">
+          <img src="/assets/Blogimage1.png" alt="News Image" />
+          <div className="imagetext">Trash Trek: Community Cleanup and <br/> Recycling Drive</div>
+        </div>
+        <div className="sm-text-container">
+          <div className="sm-row">
+            <div className="sm-row-item">
+              <img src="/assets/Blogimage2.png" alt="News Image" />
+                  <div className='sm-blogandp'>
+                      <div className='sm-blog-title'>
+                          <p >Blog</p>
+                          <p className='sm-date'>21 FEB 2024</p>
+                      </div>
+                      <div className='sm-btit'>
+                      <p>10 Tips for Effective Waste Sorting at Home</p>
+                      </div>
+                  </div>
+            </div>
+          </div>
+          
+        
+        </div>
+    </div>
   </div>
 
     </>
