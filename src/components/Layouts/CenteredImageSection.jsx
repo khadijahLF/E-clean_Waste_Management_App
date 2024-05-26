b18d0565042d85737814206997be93de2a8841be
khadijah <phunmilawal@gmail.com>
2024-05-26 19:36:12 +0100

    feat: responsiveness for mobile

--- src/components/Layouts/CenteredImageSection.jsx
+++ src/components/Layouts/CenteredImageSection.jsx
@@ -68,63 +68,63 @@ const CenteredImageSection = ({ imageSrc, leftText, rightText }) => {
             <hr />
         </div>
 
-<div className='sm-title-center'>
-    <h1>Simplified Waste Management <br/> and Recycling</h1>
-</div>
-
-
-<div className="sm-centered-image-section">
-
-
-    <div className="sm-left-text">
-        
-        <div className='sm-left-items'>
-        
-                <div className='sm-left1'>
-                    <div className='box1'>01</div>
-                    <div className='sm-left-text'>
-                        <h3>User-Friendly Interface</h3>
-                        <p>Our app is designed with simplicity and ease of use in mind. You can easily manage your waste materials and transactions with just a few taps.</p>
-                    </div>
-                </div>
-
-                <div className='sm-left1'>
-                    <div className='box1'>02</div>
-                    <div className='sm-left-text'>
-                        <h3>Real-Time Notifications</h3>
-                        <p>Stay informed and connected with real-time notifications from the app. Receive instant alerts on notifications when your scheduled pickups are confirmed. </p>
-                    </div>
-                </div>
-
-                
-                <div className='sm-left1'>
-                        <div className='box1'>03</div>
-                        <div className='sm-left-text'>
-                            <h3>Secure Transactions</h3>
-                            <p>Rest assured that your transactions within the app are safe and secure. Your payments are protected using robust security measures</p>
-                        </div>
-                </div>
-                <div className='sm-left1'>
-                        <div className='box1'>04</div>
-                        <div className='sm-left-text'>
-                            <h3>24/7 Support</h3>
-                            <p>Have questions or need assistance at any time? We offer 24/7 support to address your queries and provide assistance whenever you need it.</p>
-                        </div>
-                </div>
-
-                
-                
+        <div className='sm-title-center'>
+            <h1>Simplified Waste Management <br/> and Recycling</h1>
         </div>
-    </div>
 
-    <div className="sm-centerImg">
-        <img src="/assets/Group 99.png" alt="Centered Image" />
-    </div>
 
-   
+        <div className="sm-centered-image-section">
 
-</div>
-</div>
+
+            <div className="sm-left-text">
+                
+                <div className='sm-left-items'>
+                
+                        <div className='sm-left1'>
+                            <div className='box1'>01</div>
+                            <div className='sm-left-text'>
+                                <h3>User-Friendly Interface</h3>
+                                <p>Our app is designed with simplicity and ease of use in mind. You can easily manage your waste materials and transactions with just a few taps.</p>
+                            </div>
+                        </div>
+
+                        <div className='sm-left1'>
+                            <div className='box1'>02</div>
+                            <div className='sm-left-text'>
+                                <h3>Real-Time Notifications</h3>
+                                <p>Stay informed and connected with real-time notifications from the app. Receive instant alerts on notifications when your scheduled pickups are confirmed. </p>
+                            </div>
+                        </div>
+
+                        
+                        <div className='sm-left1'>
+                                <div className='box1'>03</div>
+                                <div className='sm-left-text'>
+                                    <h3>Secure Transactions</h3>
+                                    <p>Rest assured that your transactions within the app are safe and secure. Your payments are protected using robust security measures</p>
+                                </div>
+                        </div>
+                        <div className='sm-left1'>
+                                <div className='box1'>04</div>
+                                <div className='sm-left-text'>
+                                    <h3>24/7 Support</h3>
+                                    <p>Have questions or need assistance at any time? We offer 24/7 support to address your queries and provide assistance whenever you need it.</p>
+                                </div>
+                        </div>
+
+                        
+                        
+                </div>
+            </div>
+
+            <div className="sm-centerImg">
+                <img src="/assets/Group 99.png" alt="Centered Image" />
+            </div>
+
+        
+
+        </div>
+        </div>
 
     </>
   );
