d8ccd6f3560578e93786d366c348faa0f6e651f7
khadijah <phunmilawal@gmail.com>
2024-05-26 21:25:34 +0100

    feat: responsiveness for mobile

--- src/components/Layouts/Dispose.jsx
+++ src/components/Layouts/Dispose.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Dispose = () =>  {
   return (
     <div className="container-dis">
-    <div className="content-dis">
-      <h1>Dispose Waste & Recycle on the go</h1>
-      <p>Our mobile app makes it easy to <span>IDENTIFY,</span> <span>SORT,</span> <span>BUY,</span> <span>SELL,</span> and <br/> <span>DISPOSE</span> of waste from anywhere.</p>
-      <div className="images">
-        <img src="/assets/App Store Black Border.png" alt="Apple Download" />
-        <img src="/assets/Google Play Black Border.png" alt="Google Download" />
+      <div className="content-dis">
+        <h1>Dispose Waste & Recycle on the go</h1>
+        <p>Our mobile app makes it easy to <span>IDENTIFY,</span> <span>SORT,</span> <span>BUY,</span> <span>SELL,</span> and <br/> <span>DISPOSE</span> of waste from anywhere.</p>
+        <div className="images">
+          <img src="/assets/App Store Black Border.png" alt="Apple Download" />
+          <img src="/assets/Google Play Black Border.png" alt="Google Download" />
+        </div>
       </div>
-    </div>
   </div>
   )
 }
